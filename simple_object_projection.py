34f85abbb63c4215b941d120bc558e362d928f7a
Fynn <fynn.mannack@gmail.com>
2024-01-16 17:21:17 +0100

    implement face creation

--- simple_object_projection.py
+++ simple_object_projection.py
@@ -7,12 +7,13 @@ import pygame
 
 # Initialization of camera and window variables
 # These variables can be changed without any hesitations
-fov = 100
+fov = 70
 far = 10
 near = 0.1
 speed = 0.01
 draw_vertices = True
 draw_edges = True
+draw_faces = True
 
 # The following list 'points' contains arrays representing the coordinates of each vertex.
 # Each array is a 4x1 column vector, with the first three elements representing (x, y, z) coordinates,
@@ -28,10 +29,17 @@ points = [np.array([[0], [0], [0], [1]], dtype=np.double),
           np.array([[1], [1], [1], [1]], dtype=np.double),
           ]
 
-# The 'point_connections' array defines connections between vertices.
+# The 'point_connections' array defines connections between vertices to create edges.
 # Use the index of the vertices from the 'points' list.
 # Note: Indexing starts from 1 for better understandability.
-point_connections = [[1, 2], [1, 3], [2, 4], [3, 4], [1, 5], [2, 6], [3, 7], [4, 8], [5, 6], [5, 7], [6, 8], [7, 8]]
+point_connections = [[1, 2], [1, 3], [2, 4], [3, 4], [1, 5], [
+    2, 6], [3, 7], [4, 8], [5, 6], [5, 7], [6, 8], [7, 8]]
+
+# The 'face_connections' array defines the vertices used for creating faces
+# Use the index of the vertices from the 'points' list.
+# Note: Indexing starts from 1 for better understandability.
+face_connections = [[1, 2, 4, 3], [5, 6, 8, 7], [
+    1, 2, 6, 5], [3, 4, 8, 7], [1, 3, 7, 5], [2, 4, 8, 6]]
 
 # Window settings
 window_size = [800, 600]
@@ -39,7 +47,7 @@ aspect_ratio = window_size[0] / window_size[1]
 
 # Function for creating a projection matrix
 def make_projection_matrix(aspect_ratio, fov, far, near):
-    focal_length = 1 / math.tan(math.radians(0.25*fov))
+    focal_length = 1 / math.tan(math.radians(0.5*fov))
     projection_matrix = np.array([
         [-focal_length / aspect_ratio, 0, 0, 0],
         [0, focal_length, 0, 0],
@@ -47,6 +55,7 @@ def make_projection_matrix(aspect_ratio, fov, far, near):
         [0, 0, -1, 0]
     ])
     return projection_matrix
+
 
 pygame.init()
 screen = pygame.display.set_mode((window_size[0], window_size[1]))
@@ -56,7 +65,7 @@ projection_matrix = make_projection_matrix(aspect_ratio, fov, far, near)
 
 # create an offset between camera and object
 for i in range(len(points)):
-    points[i][2] += 2
+    points[i][2] += 5
 
 done = False
 
@@ -96,26 +105,38 @@ while not done:
         else:
             calculated_points.append([calc[0], calc[1], 0])
 
+    # When defined in 'draw_faces' draw faces
+    if draw_faces == True:
+        for face in face_connections:
+            face_points = []
+            is_clipping = False
+            for point in face:
+                if calculated_points[point-1][2] == 0:
+                    is_clipping = True
+                face_points.append((calculated_points[point-1][0] * aspect_ratio * window_size[0] + 0.5 *
+                                   window_size[0], calculated_points[point-1][1] * window_size[0] + 0.5 * window_size[1]))
+            if not is_clipping:
+                pygame.draw.polygon(screen, (0, 200, 200), face_points)
 
     # When defined in 'draw_vertices' draw vertices
     if draw_vertices == True:
         for point in calculated_points:
-            pos = (int(point[0] * aspect_ratio * 0.5 * window_size[0] + 0.5 * window_size[0]),
-                int(point[1] * 0.5 * window_size[0] + 0.5 * window_size[1]))
+            pos = (int(point[0] * aspect_ratio * window_size[0] + 0.5 * window_size[0]),
+                   int(point[1] * window_size[0] + 0.5 * window_size[1]))
             if point[2] == 1:
                 pygame.draw.circle(screen, (255, 0, 0), pos, 5)
 
     # When defined in 'draw_edges' draw edges
     if draw_edges == True:
         for x in point_connections:
-            pos1 = (calculated_points[x[0]-1][0] * aspect_ratio * 0.5 * window_size[0] + 0.5 *
-                    window_size[0], calculated_points[x[0]-1][1] * 0.5 * window_size[0] + 0.5 * window_size[1])
-            pos2 = (calculated_points[x[1]-1][0] * aspect_ratio * 0.5 * window_size[0] + 0.5 *
-                    window_size[0], calculated_points[x[1]-1][1] * 0.5 * window_size[0] + 0.5 * window_size[1])
+            pos1 = (calculated_points[x[0]-1][0] * aspect_ratio * window_size[0] + 0.5 *
+                    window_size[0], calculated_points[x[0]-1][1] * window_size[0] + 0.5 * window_size[1])
+            pos2 = (calculated_points[x[1]-1][0] * aspect_ratio * window_size[0] + 0.5 *
+                    window_size[0], calculated_points[x[1]-1][1] * window_size[0] + 0.5 * window_size[1])
             if calculated_points[x[0]-1][2] == 1 and calculated_points[x[1]-1][2] == 1:
                 pygame.draw.line(screen, (0, 0, 0), pos1, pos2)
 
-    pygame.display.flip() # Rerender the screen
-    clock.tick(30) # Framerate of 30 fps
+    pygame.display.flip()  # Rerender the screen
+    clock.tick(30)  # Framerate of 30 fps
 
 pygame.quit()
